967aae86fa5a901a6174eba1a701285cbbd500a1
RHellenes <rene.hellenes@gmail.com>
2016-08-22 09:17:43 +0200

    Hover check
    The hover effect is made, the next part now is to make a button who
    clears the color.

--- js/application.js
+++ js/application.js
@@ -2,12 +2,15 @@ $(document).ready(function(){
 		
 		
 		for (var i = 1; i < 257; i++){
-			$('main').append('<div class="draw"></div>')
+			$('main').append('<div class="draw"></div>');
 		}
 
 		var square = $('.draw').width();
 		$('.draw').height(square);
 
+		$('main').on('mouseover', '.draw', function(){
+			$(this).toggleClass('color');
+
+		});
 		
-	
 });
